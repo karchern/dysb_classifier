2f558d667acfd21f0c05cb50c0eeddf4a0166a64
Nicolai Karcher <karchernic@gmail.com>
2022-09-12 13:29:53 +0200

    finishing initial version

--- scripts/packages.r
+++ scripts/packages.r
@@ -6,3 +6,18 @@ library(SIAMCAT)
 library(data.table)
 library(vegan)
 library(RColorBrewer)
+
+parseArgs <- function() {
+
+    ## Collect arguments
+    args <- commandArgs(TRUE)
+
+    ## Parse arguments (we expect the form --arg=value)
+    parseArgs <- function(x) strsplit(sub("^--", "", x), "=")
+    argsDF <- as.data.frame(do.call("rbind", parseArgs(args)))
+    argsL <- as.list(as.character(argsDF$V2))
+    names(argsL) <- argsDF$V1
+
+    return(argsL)
+
+}
